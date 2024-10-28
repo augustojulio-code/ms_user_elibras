548505f755e6d73e5f55a339b81de9a598f39215
Julio <julio.mech@hotmail.com>
2024-10-28 00:45:24 -0300

    refactor

--- src/main/java/com/user/ms_user/adapters/controllers/SonarCloudController.java
+++ src/main/java/com/user/ms_user/adapters/controllers/SonarCloudController.java
@@ -1,7 +1,6 @@
 package com.user.ms_user.adapters.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
