e68893d67ce183b1c80638a99a9fc32a40cce4c9
Julio <julio.mech@hotmail.com>
2024-11-01 19:27:23 -0300

    Set up .env file on pipeline

--- src/test/java/com/user/ms_user/MsUserApplicationTests.java
+++ src/test/java/com/user/ms_user/MsUserApplicationTests.java
@@ -1,21 +1,25 @@
 package com.user.ms_user;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.user.ms_user.utils.EnvLoader;
 
+@SpringBootTest
 class MsUserApplicationTests {
 
 	@BeforeAll
 	public static void setup() {
-
+		EnvLoader.loadEnv();
 	}
 
 	@Test
 	void contextLoads() {
 
+		assertTrue(true);
 	}
 
 }
